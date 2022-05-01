19ab5a513cfc91de9d9da353f54e28f79f195447
ivancg86 <ivancg86@gmail.com>
2022-05-01 02:23:10 +0200

    nuevos archivos

---
+++ cookies.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        setcookie("nombre", "Ivan");
+        setcookie("edad", "34");
+    ?>
+
+    <a href="cookies2.php">Ir a segunda</a>
+</body>
+</html>
